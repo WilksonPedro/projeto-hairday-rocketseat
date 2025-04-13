3654c4f56dc316dbf613a6f799e6e59e39855d02
wilkson pedro da Silva <wil.thydus@gmail.com>
2025-04-13 20:37:38 -0300

    Adiciona HtmlWebpackPlugin e configura arquivos HTML e SVG para o projeto

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 // Path fara a importação para resolver possiveis problemas de importação
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// HtmlWebpackPlugin fara a importação do html-webpack-plugin
 
 module.exports = {
     target: "web",
@@ -21,5 +23,13 @@ module.exports = {
         // Define se o servidor vai abrir o navegador automaticamente
         liveReload: true,
         // Define se o servidor vai atualizar a página automaticamente
-    }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "index.html"),
+            favicon: path.resolve("src", "assets", "scissors.svg"),
+            // para importar o favicon
+        }),
+    ],
 }
